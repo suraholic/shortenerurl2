df99fca4a91fa3915d79704205b880579d9ced0a
suraholic <suraholic@gmail.com>
2017-09-14 16:39:05 +0900

    mysql - knex 연동, dotenv 사용

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cookieSession = require('cookie-session')
 const bodyParser = require('body-parser')
 
 const query = require('./query')
+const knex = require('./knex')
 
 const app = express()
 const urlencodedMiddleware = bodyParser.urlencoded({ extended: false })
@@ -13,6 +14,52 @@ app.use(cookieSession({
 }))
 app.set('view engine', 'ejs')
 
+function authMiddleware(req, res, next){
+  if(req.session.id){
+    knex('user')
+      .where({id: req.session.id})
+      .first()
+      .then(matched => {
+        req.user = matched
+        res.locals.user = matched
+        next()
+      })
+  } else {
+    res.redirect('/login')
+  }
+
+}
+
+app.get('/', authMiddleware, (req, res)=> {
+  knex('url_entry')
+    .where({user_id: req.user.id})
+    .then(rows=>{
+      res.render('index.ejs', {rows})
+    })
+})
+
+app.get('/login', (req, res)=>{
+  res.render('login.ejs')
+})
+
+app.post('/login', urlencodedMiddleware, (req, res)=>{
+  knex('user')
+    .where({
+      id: req.body.username,
+      password: req.body.password
+    })
+    .first()
+    .then(matched => {
+      if(matched){
+        req.session.id = matched.id
+        res.redirect('/')
+      } else {
+        res.status(400)
+        res.send('400 Bad Request')
+      }
+    })
+})
+/*
 function authMiddleware(req, res, next) {
   if (req.session.user_id) {
     query.user.get(req.session.user_id).then(user => {
@@ -65,7 +112,7 @@ app.get('/:short_url', (req, res) => {
       }
     })
 })
-
+*/
 app.listen(3000, () => {
   console.log('listening...')
 })
--- src/seed.js
+++ src/seed.js
@@ -2,3 +2,35 @@ const faker = require('faker')
 const randomstring = require('randomstring')
 
 const knex = require('./knex')
+
+knex('user')
+  .insert({
+    id: 'fast',
+    password: 'campus'
+  })
+  .then(()=>{
+    for(var i=0; i<20; i++){
+      knex('url_entry')
+        .insert({
+          id: randomstring.generate(8),
+          long_url: faker.internet.url()
+        }).then(console.log)
+    }
+  })
+
+/*
+knex('user')
+.insert({
+  id: 'fast',
+  password: 'campus'
+})
+.then(()=>{
+  for(var i=0; i<20; i++){
+    await('url_entry')
+      .insert({
+        id: randomstring.generate(8),
+        long_url: faker.internet.url()
+      }).then(console.log)
+  }
+})
+*/
